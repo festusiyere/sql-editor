d9fbbad3f70d8fbc7bf0f8e30155d23d5192c6a0
Festus Iyere <ifeanyifestus@gmail.com>
2021-08-22 23:10:25 +0100

    fix bug on router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import DefaultLayoutComponent from '@/layouts/default.vue'
 import HomeViewComponent from '@/views/home.vue'
+import DefaultLayoutComponent from '@/layouts/default.vue'
 
 Vue.use(VueRouter)
 
